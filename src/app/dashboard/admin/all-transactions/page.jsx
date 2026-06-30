7d32ebf82a4af9837474dfc6718b3886ce03ba94
anika557 <ummehabiba19758@gmail.com>
2026-06-30 23:08:59 +0600

    transistor

--- src/app/dashboard/admin/all-transactions/page.jsx
+++ src/app/dashboard/admin/all-transactions/page.jsx
@@ -2,24 +2,22 @@
 
 import { useEffect, useState } from "react";
 import { getTransactions } from "@/lib/api/hiring";
+import TransactionsTable from "@/components/dashboard/admin/TransactionsTable";
 
 export default function AllTransactions() {
   const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
     getTransactions().then((data) => {
-      console.log("DATA:", data);
       setTransactions(data);
     });
   }, []);
 
   return (
     <div>
-      <h1>Transactions</h1>
+      <h2 className="text-3xl font-bold mb-6">All Transactions</h2>
 
-      <p>Total = {transactions.length}</p>
-
-      <pre>{JSON.stringify(transactions, null, 2)}</pre>
+      <TransactionsTable transactions={transactions} />
     </div>
   );
 }
